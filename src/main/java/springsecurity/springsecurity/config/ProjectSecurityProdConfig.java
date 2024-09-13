d530961d990602159b503a54c89ffbac4adf5d4d
Anton Rudnikovskiy <mkasadasa@yandex.ru>
2024-09-13 21:39:34 +0300

    reworked

--- src/main/java/springsecurity/springsecurity/config/ProjectSecurityProdConfig.java
+++ src/main/java/springsecurity/springsecurity/config/ProjectSecurityProdConfig.java
@@ -2,26 +2,31 @@ package springsecurity.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import springsecurity.springsecurity.exceptionhandling.CustomAccessDeniedHandler;
+import springsecurity.springsecurity.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-public class ProjectSecurityConfig {
-
+@Profile("prod")
+public class ProjectSecurityProdConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
+        http.requiresChannel(rcc -> rcc.anyRequest().requiresSecure()) // Only HTTPS
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                 .requestMatchers("/notices", "/contact", "error", "register").permitAll());
         http.formLogin(withDefaults());
-        http.httpBasic(withDefaults());
+        http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
         return http.build();
     }
 
